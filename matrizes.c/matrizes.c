272c8e8d0780f255aa2738bb7a2abd167dc4f447
Dairon Romano <daironromano@gmail.com>
2023-05-16 23:05:34 -0300

    Soma, Sub e Prod.Esc.

--- matrizes.c/matrizes.c
+++ matrizes.c/matrizes.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// PROTOTIPOS DAS FUNCOES
+/// PROTOTIPOS DAS FUNCOES
 void imprimirMatriz(int** matriz);
 void imprimirVetor(int* vetor);
-void somaMatriz(int m1[3][3], int m2[3][3], int resultado[3][3]);
-void subtracaoMatriz(int m1[3][3], int m2[3][3], int resultado[3][3]);
-int produtoEscalar(int v1[], int v2[], int tamanho);
+void somaMatriz(int** m1, int** m2, int** resultadoSoma);
+void subtracaoMatriz(int** m1, int** m2, int** resultadoSub);
+int produtoEscalar(int* v1, int* v2, int tamanho);
 
-//ESTRUTURA DE DADOS
+///ESTRUTURA DE DADOS COMPLEXO
     typedef struct{
         float real;
         float imag;
     } MatrizComplexo;
 
-// FUNCAO PRINCIPAL
+/// FUNCAO PRINCIPAL
 int main(){
-    // TESTE MATRIZ SOMA
-        int** m1 = (int**)malloc(sizeof(int*) * 3);
-        int** m2 = (int**)malloc(sizeof(int*) * 3);
-        int** resultadoSoma = (int**)malloc(sizeof(int*) * 3);
+    /// TESTE MATRIZ SOMA E SUBTRACAO
+        /// ALOCACAO DE MEMORIA
+         int linhas = 3;
+         int colunas =3;
 
-        for(int i=0;i<3;i++){
-            m1[i] = (int*)malloc(sizeof(int) * 3);
-            m2[i] = (int*)malloc(sizeof(int) * 3);
-            resultadoSoma[i] = (int*)malloc(sizeof(int) * 3);
+        int** m1 = (int**)malloc(sizeof(int*) * linhas);
+        int** m2 = (int**)malloc(sizeof(int*) * linhas);
+        int** resultadoSoma = (int**)malloc(sizeof(int*) * linhas);
+        int** resultadoSub = (int**)malloc(sizeof(int*) * linhas);
+
+        for(int i=0;i<linhas;i++){
+            m1[i] = (int*)malloc(sizeof(int) * colunas);
+            m2[i] = (int*)malloc(sizeof(int) * colunas);
+            resultadoSoma[i] = (int*)malloc(sizeof(int) * colunas);
+            resultadoSub[i] = (int*)malloc(sizeof(int) * colunas);
         }
 
         m1[0][0] = 1; m1[0][1] = 2; m1[0][2] = 3;
@@ -35,7 +41,7 @@ int main(){
         m2[1][0] = 4; m2[1][1] = 5; m2[1][2] = 6;
         m2[2][0] = 7; m2[2][1] = 8; m2[2][2] = 9;
 
-        printf("======Teste da Operação Soma======\n");
+        printf("======TESTE DA OPERACAO SOMA======\n");
         printf("Operando A: \n");
         imprimirMatriz(m1);
         printf("Operando B: \n");
@@ -45,12 +51,8 @@ int main(){
         somaMatriz(m1, m2, resultadoSoma);
         imprimirMatriz(resultadoSoma);
 printf("\n");
-    // TESTE MATRIZ SUBTRACAO
-    int** resultadoSub = (int**)malloc(sizeof(int*) * 3);
-    for(int i=0;i<3;i++){
-        resultadoSub[i] = (int*)malloc(sizeof(int) * 3);
-    }
-        printf("======Teste da Operação Subtracao======\n");
+    /// TESTE MATRIZ SUBTRACAO
+        printf("======TESTE DA OPERACAO SUBTRACAO======\n");
         printf("Operando A: \n");
         imprimirMatriz(m1);
         printf("Operando B: \n");
@@ -59,8 +61,8 @@ printf("\n");
         subtracaoMatriz(m1, m2, resultadoSub);
         imprimirMatriz(resultadoSub);
 printf("\n");
-    // TESTE PRODUTO_ESCALAR
-        printf("======Teste da Operacao Produto_Escalar======\n");
+    /// TESTE PRODUTO_ESCALAR
+        printf("======TESTE DA OPERACAO PRODUTO_ESCALAR======\n");
         int* v1 = (int*)malloc(3*sizeof(int));
         int* v2 = (int*)malloc(3*sizeof(int));
 
@@ -75,7 +77,7 @@ printf("\n");
         int vetoresultado = produtoEscalar(v1,v2,3);
         printf("Resultado:\n%i", vetoresultado);
 
-    //LIBERAR MEMORIA
+    /// LIBERAR MEMORIA
     for(int i=0;i<3;i++){
         free(m1[i]);
         free(m2[i]);
@@ -91,7 +93,7 @@ printf("\n");
 
     return 0;
 }
-//FUNCAO IMPRIMIR MATRIZES
+/// FUNCAO IMPRIMIR MATRIZES
 void imprimirMatriz(int** matriz){
     for(int linhas=0; linhas<3; linhas++) {
         for(int colunas=0;colunas<3;colunas++) {
@@ -100,7 +102,7 @@ void imprimirMatriz(int** matriz){
         printf("\n");
     }
 }
-    //FUNCAO IMPRIMIR VETOR
+    /// FUNCAO IMPRIMIR VETOR
 void imprimirVetor(int* vetor){
     for(int i=0;i<3;i++){
         printf("%2i ", vetor[i]);
@@ -108,9 +110,9 @@ void imprimirVetor(int* vetor){
     printf("\n");
 }
 
-//FUNCOES IMPLEMENTADAS
-    //FUNCAO SOMA
-void somaMatriz(int m1[3][3], int m2[3][3], int resultadoSoma[3][3]) {
+/// FUNCOES IMPLEMENTADAS
+    /// FUNCAO SOMA
+void somaMatriz(int** m1, int** m2, int** resultadoSoma){
     for (int linhas=0;linhas<3; linhas++) {
         for (int colunas = 0; colunas< 3;colunas++) {
             resultadoSoma[linhas][colunas] = m1[linhas][colunas] + m2[linhas][colunas];
@@ -118,16 +120,16 @@ void somaMatriz(int m1[3][3], int m2[3][3], int resultadoSoma[3][3]) {
     }
 
 }
-    //FUNCAO SUBTRACAO
-void subtracaoMatriz(int m1[3][3], int m2[3][3], int resultadoSub[3][3]) {
+    /// FUNCAO SUBTRACAO
+void subtracaoMatriz(int** m1, int** m2, int** resultadoSub){
     for (int linhas=0;linhas<3; linhas++) {
         for (int colunas = 0; colunas< 3;colunas++) {
             resultadoSub[linhas][colunas] = m1[linhas][colunas] - m2[linhas][colunas];
         }
     }
 }
-    //FUNCAO PRODUTO_ESCALAR
-int produtoEscalar(int v1[], int v2[], int tamanho){
+    /// FUNCAO PRODUTO_ESCALAR
+int produtoEscalar(int* v1, int* v2, int tamanho){
     int vetoresultado = 0;
         for(int i=0;i<tamanho; i++){
             vetoresultado += v1[i] * v2[i];
--- matrizes.h/matrizes.h
+++ matrizes.h/matrizes.h
@@ -10,12 +10,14 @@ int main()
     } MatrizComplexo;
 
     //ASSINATURAS
+    void imprimirMatriz(int** matriz);
+    void imprimirVetor(int* vetor);
     void transposta();
     void conjugada();
     void hermitiana();
     void somaMatriz(int m1[3][3], int m2[3][3], int resultado[3][3]);
     void subtracaoMatriz(int m1[3][3], int m2[3][3], int resultado[3][3]);
-    void produtoEscalar();
+    int produtoEscalar(int v1[], int v2[], int tamanho);
     void produtoMatricial();
 
 
