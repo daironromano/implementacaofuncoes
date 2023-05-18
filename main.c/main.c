0594b087dd4c4d06b7b61170e6e6d9433a574c5c
ygor <rcarneiro860@gmail.com>
2023-05-18 13:25:55 -0300

    Nome e Matricula

--- main.c/main.c
+++ main.c/main.c
@@ -6,6 +6,8 @@ int main()
     //NOMES
     printf("Nome: Dairon Romano da Silva\n");
     printf("Matricula: 202206840032\n");
+    printf("Nome: Ygor Ramon da Silva Carneiro\n");
+    printf("Matricula: 202207040039\n");
 
     // TESTE TODAS AS FUNÇÕES
 
