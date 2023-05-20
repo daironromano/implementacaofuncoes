5bf05db6781cc2ef4c7358a8a71b87075eefbf88
Dairon Romano <daironromano@gmail.com>
2023-05-19 21:41:33 -0300

    Correção Adicional: Organização de matrizes.c

---
+++ main/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrizes.h"
+
+void testeTodos();
+int main()
+{
+    ///NOMES
+    printf("Nome: Dairon Romano da Silva\n");
+    printf("Matricula: 202206840032\n");
+    printf("Nome: Ygor Ramon da Silva Carneiro\n");
+    printf("Matricula: 202207040039\n");
+    printf("Nome: Joao Vitor da S. Sacramento\n");
+    printf("Matricula: 202206840001\n");
+
+    /// TESTE TODAS AS FUNCOEES
+
+   testeTodos();
+
+    return 0;
+}
