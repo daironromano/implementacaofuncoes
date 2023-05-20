aedb6df2e2965cca80f791e6c6c4b64f48d12faf
Dairon Romano <daironromano@gmail.com>
2023-05-20 12:35:08 -0300

    Nome adicionado

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ int main()
     printf("Nome: Dairon Romano da Silva\n");
     printf("Nome: Ygor Ramon da Silva Carneiro\n");
     printf("Nome: Joao Vitor da S. Sacramento\n");
+    printf("Nome: Douglas Maciel e Maciel\n");
 
     /// TESTE TODAS AS FUNCOEES
-
     testeTodos();
 
     return 0;
