da858b9a3c74c687148d001f4071e8e8ba8ba932
Dairon Romano <daironromano@gmail.com>
2023-05-19 23:03:49 -0300

    Correção Adicional: Organização

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrizes.h"
+
+void testeTodos();
+int main()
+{
+    ///NOMES
+    printf("==================NOMES====================\n");
+    printf("Nome: Dairon Romano da Silva\n");
+    printf("Nome: Ygor Ramon da Silva Carneiro\n");
+    printf("Nome: Joao Vitor da S. Sacramento\n");
+
+    /// TESTE TODAS AS FUNCOEES
+
+    testeTodos();
+
+    return 0;
+}
---
+++ matrizes.h
@@ -0,0 +1,26 @@
+#ifndef MATRIZES_H
+#define MATRIZES_H
+
+    ///ESTRUTURA DE DADOS COMPLEXO
+    typedef struct{
+        float real;
+        float imag;
+        int linhas;
+        int colunas;
+    } Complexo;
+
+
+    ///ASSINATURAS
+    void imprimirMatriz(Complexo** matriz, int linhas, int colunas);
+    void imprimirVetor(Complexo* vetor, int linhas);
+    Complexo** matrizTransposta(Complexo** matriz, int linhas, int colunas);
+    Complexo** matrizConjugada(Complexo** matriz, int linhas, int colunas);
+    Complexo** matrizHermitiana(Complexo** matriz, int linhas, int colunas);
+    void somaMatriz(Complexo** m1, Complexo** m2, Complexo** resultadoSoma);
+    void subtracaoMatriz(Complexo** m1, Complexo** m2, Complexo** resultadoSub);
+    Complexo produtoEscalar(Complexo* v1, Complexo* v2, int tamanho);
+    void produtoMatricial(Complexo** mat1, Complexo** mat2, Complexo** resultado, int linhas1, int colunas1, int colunas2);
+    void testeTodos(void);
+
+#endif
+
